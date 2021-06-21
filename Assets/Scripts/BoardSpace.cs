7fe64ba8a44e663b4512c52f391ce5ad78a95117
Colby Terry <colbydude@voidteam.net>
2021-06-20 19:23:39 -0700

    Add simple board space script.

---
+++ Assets/Scripts/BoardSpace.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSpace : MonoBehaviour
+{
+    public GameObject NextSpace;
+    public GameObject PreviousSpace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
